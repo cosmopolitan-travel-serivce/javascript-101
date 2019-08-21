e6fa664ddd7e28fa39d057ca0b6fb751a099a34f
Kouna Fall <kouna45@outlook.com>
2019-08-21 10:16:52 -0400

    Object merge

--- src/object/ObjectUtils.ts
+++ src/object/ObjectUtils.ts
@@ -8,11 +8,7 @@ export class ObjectUtils {
    */
   static getFirstNameAndLastNameFromName (input: any): IPerson {
     // {name:'Virgine Lamesse'}
-    let name:string[] = [];
-    let keys:string[] = Object.values(input);
-    for (let i = 0;i<keys.length;i++){
-         name = keys[i].split(' ')
-    }
+    let name:string[] =input.name.split(' ');
     return {
       first_name: name.slice(0,name.length-1).join(' '),
       last_name: name[name.length-1],
